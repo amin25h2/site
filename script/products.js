13db97aa75cac2a4fba9026e502a830b96076163
Amin Hemmati <moham1384hem.com@gmail.com>
2025-09-06 22:27:07 +0330

    Create products.js

---
+++ script/products.js
@@ -0,0 +1,56 @@
+const categories = [
+    {
+        id: "security",
+        title: "ربات‌های امنیتی",
+        products: [
+            {
+                name: "Security Bot Basic",
+                img: "../assets/pg1.png",
+                desc: "مدیریت پایه سرور و فیلتر کلمات",
+                price: "300,000 تومان"
+            },
+            {
+                name: "Security Bot Pro",
+                img: "../assets/pg1.png",
+                desc: "نسخه حرفه‌ای با رول‌دهی خودکار و لاگ کامل",
+                price: "500,000 تومان"
+            }
+        ]
+    },
+    {
+        id: "music",
+        title: "ربات‌های موزیک",
+        products: [
+            {
+                name: "Music Player",
+                img: "../assets/pg2.jpg",
+                desc: "پخش موزیک با کیفیت بالا و امکانات پیشرفته",
+                price: "400,000 تومان"
+            },
+            {
+                name: "DJ Bot",
+                img: "../assets/pg2.jpg",
+                desc: "امکانات دی‌جی و پلی‌لیست اختصاصی",
+                price: "600,000 تومان"
+            }
+        ]
+    },
+    {
+        id: "fun",
+        title: "ربات‌های سرگرمی",
+        products: [
+            {
+                name: "Game Plus",
+                img: "../assets/pg3.png",
+                desc: "بازی‌ها و سرگرمی‌های متنوع",
+                price: "400,000 تومان"
+            },
+            {
+                name: "Fun Bot",
+                img: "../assets/icon.png",
+                desc: "جوک و میم برای سرگرمی کاربران",
+                price: "250,000 تومان"
+            }
+        ]
+    }
+];
